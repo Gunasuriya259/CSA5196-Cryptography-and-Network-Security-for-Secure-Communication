0b8a9b4ff17de7861fdd5b15ca9d075c8b108709
Gunasuriya259 <112631409+Gunasuriya259@users.noreply.github.com>
2023-11-23 12:51:18 +0530

    Update CBS.py

--- CBS.py
+++ CBS.py
@@ -1,5 +1,3 @@
-# Cipher Block Chaining
-
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
@@ -9,11 +7,9 @@ def cbc_encrypt(plaintext, key, iv):
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
 
-    # Apply PKCS7 padding to the plaintext
     padder = padding.PKCS7(algorithms.AES.block_size).padder()
     plaintext_padded = padder.update(plaintext) + padder.finalize()
 
-    # Encrypt the plaintext
     ciphertext = encryptor.update(plaintext_padded) + encryptor.finalize()
 
     return ciphertext
@@ -22,25 +18,20 @@ def cbc_decrypt(ciphertext, key, iv):
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
 
-    # Decrypt the ciphertext
     plaintext_padded = decryptor.update(ciphertext) + decryptor.finalize()
 
-    # Remove PKCS7 padding
     unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
     plaintext = unpadder.update(plaintext_padded) + unpadder.finalize()
 
     return plaintext
 
-# Example usage:
-key = os.urandom(16)  # 128-bit key
-iv = os.urandom(16)   # Initialization Vector (IV)
+key = os.urandom(16)
+iv = os.urandom(16)
 
 plaintext = b'This is a secret message.'
 
-# Encryption
 ciphertext = cbc_encrypt(plaintext, key, iv)
 print(f'Encrypted Text: {ciphertext.hex()}')
 
-# Decryption
 decrypted_text = cbc_decrypt(ciphertext, key, iv)
 print(f'Decrypted Text: {decrypted_text.decode("utf-8")}')
